722bc2bdb89e7e10ff8aa2b18104eefd10ebe587
wn248211 <abcdef123>
2017-05-26 17:42:36 -0300

    second commit for stage2

--- app/src/main/java/com/example/a2/passwordValidater.java
+++ app/src/main/java/com/example/a2/passwordValidater.java
@@ -17,6 +17,18 @@ public class passwordValidater {
             System.out.println("you need enter at least 8 characters long.");
             System.exit(0);
         }
+        else if(isUpperCase(password)==false){
+            System.out.println("your password must include at least one capital letter.");
+            System.exit(0);
+        }
+        else if(isMix(password)==false){
+            System.out.println("your password must include number and letters.");
+            System.exit(0);
+        }
+        else if(includeIllegal(password)==false){
+            System.out.println("your password cannot include !, @, #, $, %, ^, &, *, (, )");
+            System.exit(0);
+        }
         System.out.println("your password has been set successfully!");
     }
     public static boolean isInsensitive(String s)
@@ -35,5 +47,47 @@ public class passwordValidater {
             return true;
     }
 
-
+    public static boolean isUpperCase(String s)
+    {
+        for (int i=0; i<s.length(); i++)
+        {
+            if (Character.isUpperCase(s.charAt(i)))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static boolean isMix(String s)
+    {
+        boolean includeNumber = false;
+        boolean includeLetter = false;
+        for (int i=0; i<s.length(); i++)
+        {
+            if (Character.isDigit(s.charAt(i)))
+            {
+                includeNumber=true;
+            }
+            if (Character.isAlphabetic(s.charAt(i)))
+            {
+                includeLetter=true;
+            }
+        }
+        return (includeNumber&&includeLetter);
+    }
+    public static boolean includeIllegal(String s)
+    {
+        boolean result=true;
+        char illegal[] = {'!','@','#','$','%','^','&','*','(',')'};
+        for (int i=0; i<s.length(); i++)
+        {
+            for(int j=0; j<illegal.length;j++){
+                if (s.charAt(i)==(illegal[j]))
+                {
+                    result = false;
+                }
+            }
+        }
+        return result;
+    }
 }
