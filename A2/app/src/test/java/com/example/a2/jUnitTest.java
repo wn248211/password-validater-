f2b0f61d3293372afebc5598b216c0b1386779e0
wn248211 <abcdef123>
2017-05-26 17:02:59 -0300

    Initialization

---
+++ A2/app/src/test/java/com/example/a2/jUnitTest.java
@@ -0,0 +1,24 @@
+package com.example.a2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Wenlong on 2017/5/26.
+ */
+
+public class jUnitTest {
+
+        @Test
+        public void Password() throws Exception {
+
+            assertEquals(false,passwordValidater.isInsensitive("password"));
+        }
+        @Test
+        public void Short() throws Exception {
+
+            assertEquals(false,passwordValidater.isShort("123"));
+        }
+
+}
